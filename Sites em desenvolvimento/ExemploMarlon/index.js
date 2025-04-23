f580e69990f78f861e3fd79b264f240730ccd718
Vitor Martins <vitor.martins.dev@gmail.com>
2025-04-22 22:17:39 -0300

    Teste Menu Nav

--- Sites em desenvolvimento/ExemploMarlon/index.js
+++ Sites em desenvolvimento/ExemploMarlon/index.js
@@ -1,6 +1,18 @@
+
 function clickFontes(){
 
     const lista_link_fontes = document.getElementById("lista_link_fontes")
+    const menu_links_fontes = document.getElementById("menu_links_fontes");
+
+    if(lista_link_fontes.style.display == 'none'){
+        lista_link_fontes.style.display = 'block'
+        menu_links_fontes.innerText = "keyboard_arrow_up" 
+    }else{
+        lista_link_fontes.style.display = 'none'
+        menu_links_fontes.innerText = "keyboard_arrow_down" 
+    }
+
     
-    lista_link_fontes.style.display = 'block'
+
+
 }
